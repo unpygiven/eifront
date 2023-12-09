85346364eace71115590fa92712e80c7d145a677
unpygiven <arda.demirtas2002@gmail.com>
2023-12-09 05:47:57 +0300

    update-5

--- components/videocomponents/VideoCard.jsx
+++ components/videocomponents/VideoCard.jsx
@@ -11,13 +11,13 @@ export default function VideoCard(props)
     const imageSrc = video.photo_link
     const slug = title.replaceAll(' ', '-');
     return (
-      <a href={`/video/${video.id}/${slug}`} style={{textDecoration: 'none'}}>
         <Card style={{ width: '18rem', margin: '0 auto', marginTop: '5%' }}>
+          <a href={`/video/${video.id}/${slug}`} style={{textDecoration: 'none'}}>
           <Card.Img variant="top" src="https://e0.pxfuel.com/wallpapers/303/523/desktop-wallpaper-gibson-les-paul-guitar.jpg" />
+          </a>
           <Card.Body>
             <Card.Title>{title}</Card.Title>
           </Card.Body>
         </Card>
-      </a>
       );
 }
