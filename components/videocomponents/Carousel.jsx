5a357f6502ec612214efdf43fcfc764d204a5031
unpygiven <arda.demirtas2002@gmail.com>
2023-12-09 05:41:12 +0300

    update-2

--- components/videocomponents/Carousel.jsx
+++ components/videocomponents/Carousel.jsx
@@ -6,44 +6,49 @@ function VideoCarousel() {
   return (
     <Carousel>
       <Carousel.Item>
+      <a href={`/video/4/`} style={{textDecoration: 'none'}}>
         <img
           className="d-block w-100"
           src="https://e0.pxfuel.com/wallpapers/303/523/desktop-wallpaper-gibson-les-paul-guitar.jpg"
           alt="First slide"
         />
+        </a>
         <Carousel.Caption style={{backgroundColor: 'gray', border: 'solid black'}}>
           <p>ornek_gunun_videosu_1_aciklama</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+      <a href={`/video/3/`} style={{textDecoration: 'none'}}>
         <img
           className="d-block w-100"
           src="https://e0.pxfuel.com/wallpapers/303/523/desktop-wallpaper-gibson-les-paul-guitar.jpg"
           alt="Second slide"
         />
-
+      </a>
         <Carousel.Caption style={{backgroundColor: 'gray', border: 'solid black'}}>
         <p>ornek_gunun_videosu_2_aciklama</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+      <a href={`/video/2/`} style={{textDecoration: 'none'}}>
         <img
           className="d-block w-100"
           src="https://e0.pxfuel.com/wallpapers/303/523/desktop-wallpaper-gibson-les-paul-guitar.jpg"
           alt="Third slide"
         />
-
+      </a>
         <Carousel.Caption style={{backgroundColor: 'gray', border: 'solid black'}}>
           <p>ornek_gunun_videosu_3_aciklama</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <a href={`/video/1/`} style={{textDecoration: 'none'}}>
         <img
           className="d-block w-100"
           src="https://e0.pxfuel.com/wallpapers/303/523/desktop-wallpaper-gibson-les-paul-guitar.jpg"
           alt="Third slide"
         />
-
+        </a>
         <Carousel.Caption style={{backgroundColor: 'gray', border: 'solid black'}}>
           <p>ornek_gunun_videosu_4_aciklama</p>
         </Carousel.Caption>
