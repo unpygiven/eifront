2fbf2e6aae9ab6b7c7b60fd8f0779cea1ce3c2db
unpygiven <arda.demirtas2002@gmail.com>
2023-12-09 05:55:58 +0300

    update-6

--- components/videocomponents/Videoplay.jsx
+++ components/videocomponents/Videoplay.jsx
@@ -5,7 +5,7 @@ export default function Videoplay(props)
     const video = props.video;
     return (
   <div style={{position: 'relative', overflow: 'hidden', width: '100%', paddingTop: '56.25%'}} className="embed-responsive">
-    <iframe style={{position: 'absolute', top:'0', bottom: '0', right: '0', left: '0', width: '100%', height: '100%'}} src={video.video_link} allowFullScreen=""></iframe>
+    <iframe style={{position: 'absolute', top:'0', bottom: '0', right: '0', left: '0', width: '100%', height: '100%'}} src={video.video_link} allowFullScreen="allowfullscreen"></iframe>
   </div>
     )
 }
