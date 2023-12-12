7a204973fd79d54f1438b08e5fa7c00cb1d4e213
unpygiven <arda.demirtas2002@gmail.com>
2023-12-12 19:28:35 +0300

    ssa

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="tr">
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+      </Head>
       <body className={inter.className} style={{background: '#4A4A4A'}}>{children}</body>
     </html>
   )
