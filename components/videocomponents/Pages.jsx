0970cc2fcaa78ec523eeeaed2a30d972de3d4f2d
unpygiven <arda.demirtas2002@gmail.com>
2023-12-12 19:43:43 +0300

    asdfsa

--- components/videocomponents/Pages.jsx
+++ components/videocomponents/Pages.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import { Container } from "react-bootstrap";
 
 export default function Page(props)
 {   
@@ -20,17 +21,17 @@ export default function Page(props)
     }
 
     return (
-        <div style={{marginTop:'2%', marginBottom:'2%'}}>
+        <Container>
             <Row>
                 <Col></Col>
                 <Col>
                     <Row>
-                        <Col> <a style={{margin: '0 auto', color: 'white'}} href={`/videos/${keywords}/${prevPage}`}><center>GERİ</center></a> </Col>
+                        <Col ><a style={{margin: '0 auto', color: 'white'}} href={`/videos/${keywords}/${prevPage}`}><center>GERİ</center></a> </Col>
                         <Col><a style={{margin: '0 auto', color: 'white'}}><center>{currentPage}</center></a></Col>
                         <Col><a style={{margin: '0 auto', color: 'white'}} href={`/videos/${keywords}/${nextPage}`}><center>İLERİ</center></a> </Col>
                     </Row></Col>
                 <Col></Col>
             </Row>
-        </div>
+        </Container>
     )
 }
