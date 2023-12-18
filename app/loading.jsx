742980d1338ce3ff6d96f4972fd1a64277bd57a6
unpygiven <arda.demirtas2002@gmail.com>
2023-12-19 00:38:04 +0300

    real6

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,6 +1,8 @@
 export default function Loading()
 {
-    <>
-        <h1 style={{color:'white'}}>Bekle</h1>
-    </>
+    return (
+        <>
+            <h1 style={{color:'white'}}>Bekle</h1>
+        </>
+    )
 }
