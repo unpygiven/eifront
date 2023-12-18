085c079e938dfc95483d5b343fe6719e5ec7eb14
unpygiven <arda.demirtas2002@gmail.com>
2023-12-19 00:29:40 +0300

    real4

--- components/videocomponents/Carousel.jsx
+++ components/videocomponents/Carousel.jsx
@@ -8,16 +8,16 @@ function VideoCarousel(props) {
       {
         videosC.map(function(x) {
             return(
-<Carousel.Item id={x.id}>
-      <a href={`/video/${x.id}/`} style={{textDecoration: 'none'}}>
+<Carousel.Item id={x.video.id}>
+      <a href={`/video/${x.video.id}/`} style={{textDecoration: 'none'}}>
         <img
           className="d-block w-100"
-          src={x.photo_link}
-          alt={x.title}
+          src={x.video.photo_link}
+          alt={x.video.title}
         />
         </a>
         <Carousel.Caption style={{backgroundColor: 'gray', border: 'solid black'}}>
-          <p>{x.title}</p>
+          <p>{x.video.title}</p>
         </Carousel.Caption>
       </Carousel.Item>
             )
